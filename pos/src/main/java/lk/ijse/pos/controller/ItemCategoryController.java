7e8032c1a80946ec43b65f0d178ada1e4d8a0dfd
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-14 10:34:53 +0530

    fix bugs in ItemCategoryController class with CrossOrigins error

--- pos/src/main/java/lk/ijse/pos/controller/ItemCategoryController.java
+++ pos/src/main/java/lk/ijse/pos/controller/ItemCategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/item-categories")
 public class ItemCategoryController {
