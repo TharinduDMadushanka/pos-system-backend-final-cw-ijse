c11bc4fb153b7bef7957a912b1aa585f80311173
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-17 22:00:19 +0530

    fix bug in Cors Error in the Item section

--- pos/src/main/java/lk/ijse/pos/controller/ItemController.java
+++ pos/src/main/java/lk/ijse/pos/controller/ItemController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/items")
 public class ItemController {
 
